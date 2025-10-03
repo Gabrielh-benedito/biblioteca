55093da60f10cd463ec3f67004b3cd643f4f2712
Gabriel Benedito <gabrielh.beneditoo@gmail.com>
2025-10-03 10:40:53 -0300

    Criação do banco e tabela

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 # Sistema de Biblioteca em SQLite
+import sqlite3
+
+def criar_tabela():
+    try:
+        #Criando uma conexao chamada "biblioteca.db"
+        conexao = sqlite3.connect("bibioteca.db")
+        #Utilizando curso para execultar comandos SQL
+        cursor = conexao.cursor()
+        cursor.execute(""" 
+            CREATE TABLE IF NOT EXISTS livros(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo TEXT NOT NULL,
+                autor INTEGER,
+                ano INTEGER,
+                disponivel TEXT                      
+            )
+            """)
+
+    except Exception as erro:
+        print(f"erro ao tentar adicionar um livro {erro}")
+    finally:
+        if conexao:
+            conexao.close()
+
+criar_tabela()
