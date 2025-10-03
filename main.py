27e0b1d579c1985d5a980295593968cb55980dd6
Gabriel Benedito <gabrielh.beneditoo@gmail.com>
2025-10-03 16:04:43 -0300

    Atualização de  disponibilidade"

--- main.py
+++ main.py
@@ -1,69 +1,87 @@
 # Sistema de Biblioteca em SQLite
 import sqlite3
 
-def criar_tabela():
+# def criar_tabela():
+#     try:
+#         #Criando uma conexao chamada "biblioteca.db"
+#         conexao = sqlite3.connect("biblioteca.db")
+#         #Utilizando curso para execultar comandos SQL
+#         cursor = conexao.cursor()
+#         cursor.execute("""  
+#             CREATE TABLE IF NOT EXISTS livros(
+#                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                 titulo TEXT NOT NULL,
+#                 autor INTEGER,
+#                 ano INTEGER,
+#                 disponivel TEXT                      
+#             )
+#             """)
+
+#     except Exception as erro:
+#         print(f"erro ao tentar criar a tabela {erro}")
+#     finally:
+#         if conexao:
+#             conexao.close()
+
+# criar_tabela()
+
+# def cadastrar_livros(titulo,autor,ano):
+#     try: 
+#         conexao = sqlite3.connect("biblioteca.db")
+#         cursor = conexao.cursor()
+
+#         cursor.execute("""
+#             INSERT INTO livros (titulo,autor,ano, disponivel)
+#             VALUES (?, ?, ?, ?)
+#         """,
+#         (titulo,autor,ano, "sim")
+#         )
+#         conexao.commit()
+#     except Exception as erro:
+#         print(f"erro ao tentar cadastrar livro!{erro}")
+#     finally:
+#         if conexao:
+#             conexao.close()
+
+
+# titulo = input("Digite o titulo do livro: ").lower().strip()
+# autor = input("Digite o nome do autor do livro: ").lower().strip()
+# ano = input("Digite o ano de lançamento do livro: ").lower().strip()
+# cadastrar_livros(titulo,autor,ano)
+
+# def listar_livros():
+#     try:
+#         conexao = sqlite3.connect("biblioteca.db")
+#         cursor = conexao.cursor()
+
+#         cursor.execute("SELECT * FROM livros")
+#         for linha in cursor.fetchall():
+#             print(f"ID: {linha[0]}  | Titulo: {linha[1]} | Autor: {linha[2]} | Ano: {linha[3]}")
+#     except Exception as erro:
+#         print(f"Erro ao tentar Listar livros!")
+#     finally:
+#         if conexao:
+#             conexao.close
+
+def atualizar_disponibilidade(id_livro):
     try:
-        #Criando uma conexao chamada "biblioteca.db"
-        conexao = sqlite3.connect("bibioteca.db")
-        #Utilizando curso para execultar comandos SQL
-        cursor = conexao.cursor()
-        cursor.execute(""" 
-            CREATE TABLE IF NOT EXISTS livros(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                titulo TEXT NOT NULL,
-                autor INTEGER,
-                ano INTEGER,
-                disponivel TEXT                      
-            )
-            """)
-
-    except Exception as erro:
-        print(f"erro ao tentar adicionar um livro {erro}")
-    finally:
-        if conexao:
-            conexao.close()
-
-criar_tabela()
-
-def cadastrar_livros(titulo,autor,ano):
-    try: 
         conexao = sqlite3.connect("biblioteca.db")
         cursor = conexao.cursor()
 
-        cursor.execute("""
-            INSERT INTO livros (titulo,autor,ano)
-            VALUES (?,?,?)
-    """,
-    (titulo,autor,ano, "sim")
-)
-        conexao.commit()
-
-        if cursor.rowcount > 0:
-            print("Livro cadastrado com sucesso!!")
+        cursor.execute("SELECT disponivel FROM livros WHERE id = ?", (id_livro,))
+        resultado = cursor.fetchone()
+        if resultado[0] == "sim":
+            novo_status = "não"
         else:
-            print("O livro não pode ser cadastrado")
+            novo_status = "sim"
+
+        cursor.execute("UPDATE livros SET disponivel = ? WHERE id = ?", (novo_status, id_livro))
+        conexao.commit()
     except Exception as erro:
-        print(f"erro ao tentar cadastrar livro!{erro}")
+        print(f"ocorreu um erro {erro}")
     finally:
-        if conexao:
-            conexao.close
+        conexao.close
 
 
-titulo = input("Digite o titulo do livro: ").lower().strip()
-autor = input("Digite o nome do autor do livro  ").lower().strip()
-ano = input("Digite o ano de lançamento do livro: ").lower().strip()
-cadastrar_livros(titulo,autor,ano)
-
-def listar_livros():
-    try:
-        conexao = sqlite3.connect("biblioteca.db")
-        cursor = conexao.cursor()
-
-        cursor.execute("SELECT * FROM livros")
-        for linha in cursor.fetchall():
-            print(f"ID: {linha[0]}  | Titulo: {linha[1]} | Autor: {linha[2]} | Ano: {linha[3]}")
-    except Exception as erro:
-        print(f"Erro ao tentar Listar livros!")
-    finally:
-        if conexao:
-            conexao.close
+livro_id = input("Digite o ID do livro que deseja atualizar: ")
+atualizar_disponibilidade(livro_id)
