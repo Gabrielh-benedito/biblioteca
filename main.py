d67b88bd0cfeef5cf40d55887a1c04d7b7caa752
Gabriel Benedito <gabrielh.beneditoo@gmail.com>
2025-10-03 12:57:12 -0300

    Função Listagem de livros

--- main.py
+++ main.py
@@ -24,3 +24,27 @@ def criar_tabela():
             conexao.close()
 
 criar_tabela()
+
+def cadastrar_livros(titulo,autor,ano):
+    try: 
+        conexao = sqlite3.connect("biblioteca.db")
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+            INSERT INTO livros (titulo,autor,ano)
+            VALUES (?,?,?)
+    """,
+    (titulo,autor,ano, "sim")
+)
+        conexao.commit()
+
+        if cursor.rowcount > 0:
+            print("Livro cadastrado com sucesso!!")
+        else:
+            print("O livro não pode ser cadastrado")
+    except Exception as erro:
+        print(f"erro ao tentar cadastrar livro!{erro}")
+    finally:
+        if conexao:
+            conexao.close
+
